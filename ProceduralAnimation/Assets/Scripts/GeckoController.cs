f10be3c9a664cd0506c11b6ef644049a473e4312
Karsten Westra <karsten@moyosa.nl>
2020-01-14 11:20:10 +0100

    Added playground project with head tracked Gecko skeleton.

---
+++ ProceduralAnimation/Assets/Scripts/GeckoController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+[ExecuteAlways]
+public class GeckoController : MonoBehaviour
+{
+#pragma mark disable 649
+    [SerializeField] private Transform target;
+    [SerializeField] private Transform headBone;
+
+    [SerializeField] private float headMaxTurnAngle;
+    [SerializeField] private float headTrackingSpeed;
+#pragma mark restore 649
+    
+    private void LateUpdate()
+    {
+        UpdateHeadTracking();
+    }
+
+    private void UpdateHeadTracking()
+    {
+        Quaternion currentLocalRotation = headBone.localRotation;
+        headBone.localRotation = Quaternion.identity;
+
+        Vector3 targetWorldLookDir = target.position - headBone.position;
+        Vector3 targetLocalLookDir = headBone.InverseTransformDirection(targetWorldLookDir);
+
+        targetLocalLookDir =
+            Vector3.RotateTowards(Vector3.forward, targetLocalLookDir, Mathf.Deg2Rad * headMaxTurnAngle, 0);
+
+        // Get the local rotation by using LookRotation on a local directional vector
+        Quaternion targetLocalRotation = Quaternion.LookRotation(targetLocalLookDir, Vector3.up);
+
+        // Apply smoothing
+        headBone.localRotation = Quaternion.Slerp(
+            currentLocalRotation,
+            targetLocalRotation,
+            1 - Mathf.Exp(-headTrackingSpeed * Time.deltaTime)
+        );
+    }
+}
